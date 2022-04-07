03b916ce8a6dc622a2b505925710e0ef67ee291d
Jurgen Hasmeta <58026572+Avenger22@users.noreply.github.com>
2022-04-07 16:24:12 +0200

    fixed backend

--- prisma/setup.ts
+++ prisma/setup.ts
@@ -32,24 +32,6 @@ const users = [
   }
 ]
 
-const logins = [
-  {
-    id: 1,
-    status: "success",
-    userId: 1
-  },
-  {
-    id: 2,
-    status: "success",
-    userId: 1
-  },
-  {
-    id: 3,
-    status: "success",
-    userId: 2
-  }
-]
-
 const avatars = [
   {
     id: 1,
@@ -67,127 +49,37 @@ const videos = [
   {
     id: 1,
     title: "tikitaka",
-    countCommentsInside: 0,
-    countLikesInside: 0,
     src: "/public/uploads/videos/tikitaka.mp4",
     thumbnail: "/public/uploads/thumbnails/tikitaka.jpg",
     userId: 1,
-    categoryId: 1
+    categoryId: 2
   },
   {
     id: 2,
     title: "amazing skill look",
     description: "",
-    countCommentsInside: 0,
-    countLikesInside: 0,
     src: "/public/uploads/videos/amazing skill look.mp4",
     thumbnail: "/public/uploads/thumbnails/amazing skill look.jpg",
     userId: 2,
-    categoryId: 1
+    categoryId: 3
   },
   {
     id: 3,
     title: "skills and goals",
     description: "",
-    countCommentsInside: 0,
-    countLikesInside: 0,
     src: "/public/uploads/videos/skills and goals.mp4",
     thumbnail: "/public/uploads/thumbnails/skills and goals.jpg",
     userId: 1,
-    categoryId: 1
+    categoryId: 3
   },
   {
     id: 4,
     title: "iniesta skill",
     description: "",
-    countCommentsInside: 0,
-    countLikesInside: 0,
     src: "/public/uploads/videos/iniesta skill.mp4",
     thumbnail: "/public/uploads/thumbnails/iniesta skill.jpg",
     userId: 2,
-    categoryId: 1
-  }
-]
-
-const comments = [
-  {
-    id: 1,
-    content: "hi i am jurgen",
-    userId: 1,
-    videoId: 2,
-    countLikesInside: 0
-  },
-  {
-    id: 2,
-    content: "hi i am egon",
-    userId: 2,
-    videoId: 1,
-    countLikesInside: 0
-  }
-]
-
-const commentLikes = [
-  {
-    id: 1,
-    userId: 1,
-    commentId: 2
-  },
-  {
-    id: 2,
-    userId: 2,
-    commentId: 1
-  }
-]
-
-const commentDislikes = [
-  {
-    id: 1,
-    userId: 1,
-    commentId: 1
-  },
-  {
-    id: 2,
-    userId: 2,
-    commentId: 2
-  }
-]
-
-const videoLikes = [
-  {
-    id: 1,
-    userId: 1,
-    videoId: 2
-  },
-  {
-    id: 2,
-    userId: 2,
-    videoId: 1
-  }
-]
-
-const videoDislikes = [
-  {
-    id: 1,
-    userId: 1,
-    videoId: 1
-  },
-  {
-    id: 2,
-    userId: 2,
-    videoId: 2
-  }
-]
-
-const subscribers = [
-  {
-    id: 1,
-    subscriberId: 1,
-    subscribingId: 2
-  },
-  {
-    id: 2,
-    subscriberId: 2,
-    subscribingId: 1
+    categoryId: 4
   }
 ]
 
@@ -274,58 +166,27 @@ const hashtags = [
   }
 ]
 
-const savedVideos = [
-  {
-    id: 1,
-    userId: 1,
-    videoId: 1
-  },
-  {
-    id: 2,
-    userId: 1,
-    videoId: 2
-  },
-  {
-    id: 3,
-    userId: 1,
-    videoId: 3
-  },
-  {
-    id: 4,
-    userId: 2,
-    videoId: 3
-  },
-  {
-    id: 5,
-    userId: 2,
-    videoId: 1
-  }
-]
-
 async function createStuff () {
-
-  await prisma.commentLike.deleteMany()
-  await prisma.commentDislike.deleteMany()
 
   // @ts-ignore
   await prisma.category.deleteMany()
+
   // @ts-ignore
   await prisma.videoHashtag.deleteMany()
+
   // @ts-ignore
   await prisma.hashtag.deleteMany()
 
-  await prisma.videoLike.deleteMany()
-  await prisma.videoDislike.deleteMany()
   //@ts-ignore
-  await prisma.savedVideo.deleteMany()
-
   await prisma.login.deleteMany()
 
   //@ts-ignore
-  await prisma.subscribe.deleteMany()
   await prisma.avatar.deleteMany()
-  await prisma.comment.deleteMany()
+
+  //@ts-ignore
   await prisma.video.deleteMany()
+
+  //@ts-ignore
   await prisma.user.deleteMany()
 
   for (const user of users) {
@@ -341,32 +202,8 @@ async function createStuff () {
     await prisma.video.create({ data: video })
   }
 
-  for (const comment of comments) {
-    await prisma.comment.create({ data: comment })
-  }
-
   for (const avatar of avatars) {
     await prisma.avatar.create({ data: avatar })
-  }
-
-  for (const login of logins) {
-    await prisma.login.create({ data: login })
-  }
-
-  for (const videoLike of videoLikes) {
-    await prisma.videoLike.create({ data: videoLike })
-  }
-
-  for (const videoDislike of videoDislikes) {
-    await prisma.videoDislike.create({ data: videoDislike })
-  }
-
-  for (const commentLike of commentLikes) {
-    await prisma.commentLike.create({ data: commentLike })
-  }
-
-  for (const commentDislike of commentDislikes) {
-    await prisma.commentDislike.create({ data: commentDislike })
   }
 
   for (const hashtag of hashtags) {
@@ -377,16 +214,6 @@ async function createStuff () {
   for (const videoHashtag of videoHashtags) {
     //@ts-ignore
     await prisma.videoHashtag.create({ data: videoHashtag })
-  }
-
-  for (const subscribe of subscribers) {
-    //@ts-ignore
-    await prisma.subscribe.create({ data: subscribe })
-  }
-
-  for (const savedVideo of savedVideos) {
-    //@ts-ignore
-    await prisma.savedVideo.create({ data: savedVideo })
   }
 
 }
